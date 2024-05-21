acb0cc4c679e059d3ec895347bdef896256e08fd
teja431 <saitejaswitata3@gmail.com>
2024-05-21 23:15:45 +0530

    debug2

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 dnf install nginx -y 
+cp ./sample-frontend.conf /etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-cp ./sample-frontend.conf /etc/nginx/default.d/expense.conf
 systemctl enable nginx 
 systemctl start nginx 
