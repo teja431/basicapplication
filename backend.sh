a1d1eb16664b928579daba166da7466a725f32ba
teja431 <saitejaswitata3@gmail.com>
2024-05-21 23:25:27 +0530

    debug backend

--- backend.sh
+++ backend.sh
@@ -1,16 +1,16 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+cp ./sample-backend.conf /etc/systemd/system/backend.service
 useradd expense
 mkdir /app 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip 
 cd /app 
 unzip /tmp/backend.zip
-cd /app 
-npm install 
+npm install
 systemctl daemon-reload
+dnf install mysql -y 
+mysql -h 172-31-33-68 -uroot -pExpenseApp@1 < /app/schema/backend.sql 
 systemctl enable backend 
 systemctl start backend
-dnf install mysql -y 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql 
 
